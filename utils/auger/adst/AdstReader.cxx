bf0efca3da50a79846710f45b6c15731b70392ac
Paul Filip <paul.filip@kit.edu>
2025-03-28 21:08:44 +0100

    something was fucky here i think?

--- utils/auger/adst/AdstReader.cxx
+++ utils/auger/adst/AdstReader.cxx
@@ -1,6 +1,7 @@
 // Pauls stuff
 #include <algorithm>
 #include <iomanip>
+#include <unordered_map>
 
 // stl
 #include <iostream>
@@ -38,9 +39,24 @@
 #include <boost/tuple/tuple.hpp>
 #include <boost/tuple/tuple_io.hpp>
 
-using namespace std;
 using namespace utl;
 namespace fs = boost::filesystem;
+
+std::unordered_map<int, Double_t> showerPlaneMap = {
+  { 90000, 200 },
+  { 90001, 350 }, { 90002, 350 },
+  { 90003, 500 }, { 90004, 500 }, { 90005, 500 },
+  { 90006, 650 }, { 90007, 650 }, { 90008, 650 }, { 90009, 650 },
+  { 90010, 800 }, { 90011, 800 }, { 90012, 800 }, { 90013, 800 }, { 90014, 800 },
+  { 90015, 950 }, { 90016, 950 }, { 90017, 950 }, { 90018, 950 }, { 90019, 950 }, 
+  { 90020, 950 },
+  { 90021, 1100 }, { 90022, 1100 }, { 90023, 1100 }, { 90024, 1100 }, { 90025, 1100 }, 
+  { 90026, 1100 }, { 90027, 1100 },
+  { 90028, 1250 }, { 90029, 1250 }, { 90030, 1250 }, { 90031, 1250 }, { 90032, 1250 }, 
+  { 90033, 1250 }, { 90034, 1250 }, { 90035, 1250 },
+  { 90036, 1400 }, { 90037, 1400 }, { 90038, 1400 }, { 90039, 1400 }, { 90040, 1400 }, 
+  { 90041, 1400 }, { 90042, 1400 }, { 90043, 1400 }, { 90044, 1400 },
+};
 
 void ExtractDataFromAdstFiles(fs::path pathToAdst)
 {
@@ -83,8 +99,11 @@ void ExtractDataFromAdstFiles(fs::path pathToAdst)
     // loop over all triggered stations
     for (const auto& recStation : sdEvent.GetStationVector())
     {
+      if (!recStation.IsDense()) continue;
+
       const auto stationId = recStation.GetId();
-      const auto SPD = detectorGeometry.GetStationAxisDistance(stationId, showerAxis, showerCore);  // in m
+      // const auto SPD = detectorGeometry.GetStationAxisDistance(stationId, showerAxis, showerCore);  // in m
+      const Double_t SPD = showerPlaneMap[stationId];
       traceFile.write(reinterpret_cast<const char*>(&stationId), sizeof stationId);
       traceFile.write(reinterpret_cast<const char*>(&SPD), sizeof SPD);
 
@@ -123,8 +142,8 @@ void ExtractDataFromAdstFiles(fs::path pathToAdst)
 
 int main(int argc, char** argv) 
 {
-  int start = 2000;
-  int end =  argc;
+  int start = 4170;
+  int end = argc;
   for (int i=start; i < end; i++)
   {
     std::cout << "Processing " << i << "/" << argc-1 << ": " << argv[i] << "\n";
